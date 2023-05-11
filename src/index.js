e900b8a567d5ee034e6bae23f2ff75b4341074be
Sangaviraghavan <Sangaviraghavan2002@gmail.com>
2023-05-11 18:31:39 +0530

    New onw

--- src/index.js
+++ src/index.js
@@ -2,20 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NoPage from './NoPage';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <App/>
-    <Routes>
-      <Route>
-      <Route index element={<App/>} />
-      <Route path="home" element={<App/>} />
-      <Route path="*" element={<NoPage/>} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
 );
