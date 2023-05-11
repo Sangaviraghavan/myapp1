559fe22dbf9cdbfd8a6517d0706daf26501cf890
Sangaviraghavan <Sangaviraghavan2002@gmail.com>
2023-05-11 18:22:35 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NoPage from './NoPage';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <BrowserRouter>
+    <Routes>
+      <Route>
+      <Route index element={<App />} />
+      <Route path="home" element={<App />} />
+      <Route path="*" element={<NoPage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
