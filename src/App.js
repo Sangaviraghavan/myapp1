33a1b6e7f394ac1c93c87f40a95a5f3daae7af8c
Sangaviraghavan <Sangaviraghavan2002@gmail.com>
2023-05-11 17:42:27 +0530

    Simple

--- src/App.js
+++ src/App.js
@@ -1,26 +1,89 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import MovieBox from './MovieBox';
+import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 const API_URL = "https://api.themoviedb.org/3/movie/popular?api_key=bcc4ff10c2939665232d75d8bf0ec093";
+const API_SEARCH = "https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query";
 
 function App() {
 
-  const [movies , setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [query , setQuery] = useState('');
+
   useEffect(() => {
     fetch(API_URL)
-    .then((res)=>res.json())
-    .then(data=>{
+      .then((res) => res.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results);
+      })
+  }, []);
+
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try{
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=bcc4ff10c2939665232d75d8bf0ec093&query=${query}`;
+      const res = await fetch(url);
+      const data = await res.json();
       console.log(data);
-      setMovies(data.results);
-    })
-  },[])
+      setMovies(data.results)
+    }
+
+    catch(e){
+      console.log(e);
+    }
+  }
+
+  const changeHandler = (e) =>{
+    setQuery(e.target.value);
+  }
 
   return (
-    <div className="App">
-      {movies.map((movieReq)=><MovieBox key={movieReq.id} {...movieReq}/>)}
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg" className='mb-5'>
+        <Container className='text-white d-flex justify-content-between'>
+          <Navbar.Brand href="/home" className='text-white'>Movie DB</Navbar.Brand>
+          <Nav className="mx-auto">
+            <Nav.Link href="/home" className='text-white'>Home</Nav.Link>
+          </Nav>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className=' flex-grow-0'>
+            <Form className="d-flex" onSubmit={searchMovie}>
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+                name='query'
+                value={query} 
+                onChange={changeHandler}
+              />
+              <Button type='submit' variant="outline-success">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div className="App container">
+        {movies.length > 0 ? (
+                <div>
+                <div className='row text-center g-5'>
+                  {movies.map((movieReq) => <MovieBox key={movieReq.id} {...movieReq} />)}
+                </div>
+              </div>
+        ) : (
+          <h2 className='no'>
+            No Movies Are There
+          </h2>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/MovieBox.js
+++ src/MovieBox.js
@@ -1,14 +1,48 @@
 import React from "react"
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from "react";
 
 const API_IMG = "https://image.tmdb.org/t/p/w500/";
 
-const MovieBox = ({title , poster_path , vote_average , release_date , overview}) => {
-    return(
-        <div>
-           <h1>{title}</h1>
-           <img src={API_IMG+poster_path}></img>
-           <p>{overview}</p>
-        </div>
+const MovieBox = ({ title, poster_path, vote_average, release_date, overview }) => {
+
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+
+            <div className="col-lg-6 col-xl-3 col-sm-12 align-self-stretch">
+                <Card className="h-100">
+                    <Card.Img variant="top" src={API_IMG + poster_path} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Button variant="primary" onClick={handleShow}>view More</Button>
+                    </Card.Body>
+                </Card>
+            <Modal aria-labelledby="contained-modal-title-vcenter"
+      centered show={show} onHide={handleClose}>
+                <Modal.Header closeButton className="border-0">
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <img className="modal-img mb-3" src={API_IMG+poster_path}></img>
+                    <h2 className="fs-4 mb-0">imdb: {vote_average}</h2>
+                    <h3 className="fs-4 mb-4">Release Date: {release_date}</h3>
+                    <p className="fw-bold mb-0">overview</p>
+                    <p className="small-text mb-0">{overview}</p>
+                </Modal.Body>
+                <Modal.Footer className="border-0">
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            
+            </div>
+
     )
 }
 
